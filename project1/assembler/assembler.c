64c19e8db03f1e652ca1b17f357eb2c7db1cbddb
2018008813 <2018008813@hanyang.ac.kr>
2023-05-08 22:44:31 +0900

    project1 complete

--- project1/assembler/assembler.c
+++ project1/assembler/assembler.c
@@ -1,156 +1,26 @@
 /* Assembler code fragment for LC-2K */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #define MAXLINELENGTH 1000
-
-struct LABEL{
-    char label[MAXLINELENGTH];
-    int address;
-    struct LABEL *next;
-};
-
-struct LABEL *head;
 
 int readAndParse(FILE *, char *, char *, char *, char *, char *);
 int isNumber(char *);
+int getLabel(char *);
+void checkReg(char *);
 
-int isLetter(char *str){
-    return ((65 <= str[0] && str[0] <= 90) || (97 <= str[0] && str[0] <= 122));
-}
+struct instruction{
+    char label[MAXLINELENGTH];
+    char opcode[MAXLINELENGTH];
+    char arg0[MAXLINELENGTH];
+    char arg1[MAXLINELENGTH];
+    char arg2[MAXLINELENGTH];
+} instruction;
 
-int isSymbol(char *str){
-    for (int i = 0; i < strlen(str); i++){
-        if (48 < str[i] || (58 <= str[i] && str[i] <= 64) || (91 <= str[i] && str[i] <= 96) || 123 <= str[i]){
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void addLabel(char *label, int address){
-    struct LABEL *now;
-    struct LABEL *node = (struct LABEL *)malloc(sizeof(struct LABEL));
-    strcpy(node->label, label);
-    node->address = address;
-    node->next = NULL;
-    now = head;
-
-    if (now->next == NULL){
-        now->next = node;
-    }
-    else{
-        while (now->next != NULL){
-            now = now->next;
-            if (strcmp(now->label, label) == 0){
-                printf("error: duplicate label (%s)\n", now->label);
-                exit(1);
-            }
-        }
-        now->next = node;
-    }
-}
-
-int findLabel(char *label){
-    struct LABEL *now;
-    now = head;
-
-    if (now->next == NULL){
-        printf("error: label list empty\n");
-        exit(1);
-    }
-    else{
-        now = now->next;
-        while (1){
-            if (!strcmp(now->label, label)){
-                return now->address;
-            }
-            now = now->next;
-            if (now == NULL){
-                break;
-            }
-        }
-    }
-    printf("No such label(%s) in list\n", label);
-    exit(1);
-}
-
-int checkReg(char *reg){
-    if (isNumber(reg)){
-        return atoi(reg);
-    }
-    else{
-        return findLabel(reg);
-    }
-}
-
-int bitSize(int num, int size){
-    int n = 1;
-    for (int i = 0; i < size; i++){
-        n *= 2;
-    }
-    if (num < n){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-
-int makeRtype(int opcode, char *arg0, char *arg1, char *arg2){
-    int regA = checkReg(arg0);
-    int regB = checkReg(arg1);
-    int dest = checkReg(arg2);
-    int unused = 0;
-
-    if (bitSize(regA,3) && bitSize(regB,3) && bitSize(dest,3)){
-        return ((unused << 31) | (opcode << 22) | (regA << 19) | (regB << 16) | dest);
-    }
-    else{
-        printf("error: too many bits in R type\n");
-        exit(1);
-    }
-}
-
-int makeItype(int pc, int opcode, char *arg0, char *arg1, char *arg2){
-    int regA = checkReg(arg0);
-    int regB = checkReg(arg1);
-    int address = checkReg(arg2);
-    int unused = 0;
-    int bitmask = 0x0000FFFF;
-
-    if (bitSize(regA,3) && bitSize(regB,3) && bitSize(address,16)){
-        if (opcode == 4 && !isNumber(arg2)){
-            bitmask = bitmask & (address - (pc + 1));
-            return ((unused << 31) | (opcode << 22) | (regA << 19) | (regB << 16) | bitmask);
-        }
-        else{
-            return ((unused << 31) | (opcode << 22) | (regA << 19) | (regB << 16) | address);
-        }
-    }
-    else{
-        printf("error: too many bits in I type\n");
-        exit(1);
-    }
-}
-
-int makeJtype(int opcode, char *arg0, char *arg1){
-    int regA = checkReg(arg0);
-    int regB = checkReg(arg1);
-    int unused = 0;
-    if (bitSize(regA,3) && bitSize(regB,3)){
-        return ((unused << 31) | (opcode << 22) | (regA << 19) | (regB << 16));
-    }
-    else{
-        printf("error: too many bits in J type\n");
-        exit(1);
-    }
-}
-
-int makeOtype(int opcode){
-    int unused = 0;
-    return ((unused << 31) | (opcode << 22));
-}
+struct instruction instructions[55];
+int countline = 0;
 
 int main(int argc, char *argv[])
 {
@@ -158,10 +28,7 @@ int main(int argc, char *argv[])
     FILE *inFilePtr, *outFilePtr;
     char label[MAXLINELENGTH], opcode[MAXLINELENGTH], arg0[MAXLINELENGTH],
         arg1[MAXLINELENGTH], arg2[MAXLINELENGTH];
-    int pc = -1, len;
-
-    head = (struct LABEL *)malloc(sizeof(struct LABEL));
-    head->next = NULL;
+    int count;
 
     if (argc != 3) {
         printf("error: usage: %s <assembly-code-file> <machine-code-file>\n",
@@ -171,15 +38,13 @@ int main(int argc, char *argv[])
 
     inFileString = argv[1];
     outFileString = argv[2];
-    inFilePtr = fopen(inFileString, "r");
 
+    inFilePtr = fopen(inFileString, "r");
     if (inFilePtr == NULL) {
         printf("error in opening %s\n", inFileString);
         exit(1);
     }
-
     outFilePtr = fopen(outFileString, "w");
-
     if (outFilePtr == NULL) {
         printf("error in opening %s\n", outFileString);
         exit(1);
@@ -188,115 +53,175 @@ int main(int argc, char *argv[])
     /* here is an example for how to use readAndParse to read a line from
         inFilePtr */
     while (readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)){
-        /* reached end of file */
-        pc++;
-        len = strlen(label);
-        printf("%s", label);
-        if (len != 0){
-            if (len > 6){
-                printf("error: label(%s) is over 6 characters\n", label);
-                fclose(inFilePtr);
-                fclose(outFilePtr);
-                exit(1);
+        if(countline!=0&&strcmp(label,"")!=0){
+            for(int i=0;i<countline;i++){
+                if(!strcmp(instructions[countline].label,label)){
+                    printf("error: duplicate label\n");
+                    exit(1);
+                }
             }
-            else if (!isLetter(label)){
-                printf("error: label(%s) has to start with a letter\n", label);
-                fclose(inFilePtr);
-                fclose(outFilePtr);
-                exit(1);
-            }
-            else if (isSymbol(label)){
-                printf("error: label(%s) has to consist of letters and numbers\n", label);
-                fclose(inFilePtr);
-                fclose(outFilePtr);
-                exit(1);
-            }
-            addLabel(label, pc);
         }
+        strcpy(instructions[countline].label, label);
+        strcpy(instructions[countline].opcode, opcode);
+        strcpy(instructions[countline].arg0, arg0);
+        strcpy(instructions[countline].arg1, arg1);
+        strcpy(instructions[countline].arg2, arg2);
+        countline++;
+        /* reached end of file */
     }
 
+    /* TODO: Phase-1 label calculation */
+
     /* this is how to rewind the file ptr so that you start reading from the
-        beginning of the file */
+     beginning of the file */
+
     rewind(inFilePtr);
 
-    /* after doing a readAndParse, you may want to do the following to test the
-        opcode */
-    pc = -1;
+    /* TODO: Phase-2 generate machine codes to outfile */
     while (readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)){
-        pc++;
-        if (!strcmp(opcode, "add")){
-            fprintf(outFilePtr, "%i\n", makeRtype(0, arg0, arg1, arg2));
-        }
-        else if (!strcmp(opcode, "nor")){
-            fprintf(outFilePtr, "%i\n", makeRtype(1, arg0, arg1, arg2));
-        }
-        else if (!strcmp(opcode, "lw")){
-            fprintf(outFilePtr, "%i\n", makeItype(pc, 2, arg0, arg1, arg2));
-        }
-        else if (!strcmp(opcode, "sw")){
-            fprintf(outFilePtr, "%i\n", makeItype(pc, 3, arg0, arg1, arg2));
-        }
-        else if (!strcmp(opcode, "beq")){
-            fprintf(outFilePtr, "%i\n", makeItype(pc, 4, arg0, arg1, arg2));
-        }
-        else if (!strcmp(opcode, "jalr")){
-            fprintf(outFilePtr, "%i\n", makeJtype(5, arg0, arg1));
-        }
-        else if (!strcmp(opcode, "halt")){
-            fprintf(outFilePtr, "%i\n", makeOtype(6));
-        }
-        else if (!strcmp(opcode, "noop")){
-            fprintf(outFilePtr, "%i\n", makeOtype(7));
-        }
-        else if (!strcmp(opcode, ".fill")){
-            if (isNumber(arg0)){
-                fprintf(outFilePtr, "%i\n", atoi(arg0));
+        int x = 0;
+        /* after doing a readAndParse, you may want to do the following to test the opcode */
+        if (!strcmp(instructions[count].opcode, "add") || !strcmp(instructions[count].opcode, "nor")){
+            /* do whatever you need to do for opcode "add" */
+            if (!strcmp(instructions[count].opcode, "add")){
+                x += 0;
             }
             else{
-                fprintf(outFilePtr, "%i\n", findLabel(arg0));
+                x += 1;
+            }
+            checkReg(instructions[count].arg0);
+            x = x << 3;
+            x += atoi(instructions[count].arg0);
+            checkReg(instructions[count].arg1);
+            x = x << 3;
+            x += atoi(instructions[count].arg1);
+            x = x << 13;
+            checkReg(instructions[count].arg2);
+            x = x << 3;
+            x += atoi(instructions[count].arg2);
+        }
+        else if (!strcmp(instructions[count].opcode, "lw") || !strcmp(instructions[count].opcode, "sw") || !strcmp(instructions[count].opcode, "beq")){
+            int offset;
+            if (!strcmp(instructions[count].opcode, "lw")){
+                x += 2;
+            }
+            else if (!strcmp(instructions[count].opcode, "sw")){
+                x += 3;
+            }
+            else{
+                x += 4;
+            }
+            checkReg(instructions[count].arg0);
+            x = x << 3;
+            x += atoi(instructions[count].arg0);
+            checkReg(instructions[count].arg1);
+            x = x << 3;
+            x += atoi(instructions[count].arg1);
+            x = x << 16;
+
+            if (isNumber(instructions[count].arg2)){
+                offset = atoi(instructions[count].arg2);
+            }
+            else{
+                if (getLabel(instructions[count].arg2) == -1){
+                    printf("error: undefined label\n");
+                    exit(1);
+                }
+                offset = getLabel(instructions[count].arg2);
+                if (!strcmp(instructions[count].opcode, "beq")){
+                    offset = offset - count - 1;
+                }
+            }
+            if (offset < -32768 || 32767 < offset){
+                printf("error: offsetFields over\n");
+                exit(1);
+            }
+            offset = 0x0000ffff & offset;
+            x += offset;
+        }
+        else if (!strcmp(instructions[count].opcode, "jalr")){
+            x += 5;
+            checkReg(instructions[count].arg0);
+            x = x << 3;
+            x += atoi(instructions[count].arg0);
+            checkReg(instructions[count].arg1);
+            x = x << 3;
+            x += atoi(instructions[count].arg1);
+            x = x << 16;
+        }
+        else if (!strcmp(instructions[count].opcode, "halt") || !strcmp(instructions[count].opcode, "noop")){
+            if (!strcmp(instructions[count].opcode, "halt")){
+                x += 6;
+            }
+            else{
+                x += 7;
+            }
+            x = x << 22;
+        }
+        else if (!strcmp(instructions[count].opcode, ".fill")){
+            if (isNumber(instructions[count].arg0)){
+                x = atoi(instructions[count].arg0);
+            }
+            else{
+                x = getLabel(instructions[count].arg0);
+                if (x == -1){
+                    printf("error : undefined label\n");
+                    exit(1);
+                }
             }
         }
         else{
-            printf("error: (%s) does not match any opcode\n", opcode);
-            fclose(inFilePtr);
-            fclose(outFilePtr);
+            printf("error : unknown opcode\n");
             exit(1);
         }
+        fprintf(outFilePtr,"%d\n",x);
+        count++;
     }
-    exit(0);
-} 
+
+    if (inFilePtr){
+        fclose(inFilePtr);
+    }
+    if (outFilePtr){
+        fclose(outFilePtr);
+    }
+    return (0);
+}
 
 /*
-   * Read and parse a line of the assembly-language file.  Fields are returned
-   * in label, opcode, arg0, arg1, arg2 (these strings must have memory already
-   * allocated to them).
-   *
-   * Return values:
-   *     0 if reached end of file
-   *     1 if all went well
-   *
-   * exit(1) if line is too long.
-   */
+ * Read and parse a line of the assembly-language file.  Fields are returned
+ * in label, opcode, arg0, arg1, arg2 (these strings must have memory already
+ * allocated to them).
+ *
+ * Return values:
+ *     0 if reached end of file
+ *     1 if all went well
+ *
+ * exit(1) if line is too long.
+ */
 int readAndParse(FILE *inFilePtr, char *label, char *opcode, char *arg0,
                  char *arg1, char *arg2)
 {
     char line[MAXLINELENGTH];
     char *ptr = line;
+
     /* delete prior values */
     label[0] = opcode[0] = arg0[0] = arg1[0] = arg2[0] = '\0';
+
     /* read the line from the assembly-language file */
     if (fgets(line, MAXLINELENGTH, inFilePtr) == NULL)
     {
         /* reached end of file */
         return (0);
     }
-    /* checkReg for line too long (by looking for a \n) */
+
+    /* check for line too long (by looking for a \n) */
     if (strchr(line, '\n') == NULL)
     {
         /* line too long */
         printf("error: line too long\n");
         exit(1);
     }
+
     /* is there a label? */
     ptr = line;
     if (sscanf(ptr, "%[^\t\n\r ]", label))
@@ -304,17 +229,40 @@ int readAndParse(FILE *inFilePtr, char *label, char *opcode, char *arg0,
         /* successfully read label; advance pointer over the label */
         ptr += strlen(label);
     }
+
     /*
      * Parse the rest of the line.  Would be nice to have real regular
      * expressions, but scanf will suffice.
      */
-    sscanf(ptr, "%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]",
+    sscanf(ptr, "%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%"
+                "[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]",
            opcode, arg0, arg1, arg2);
     return (1);
 }
 
-int isNumber(char *string){ 
+int isNumber(char *string){
     /* return 1 if string is a number */
     int i;
     return ((sscanf(string, "%d", &i)) == 1);
+}
+
+int getLabel(char *string){
+    for (int i = 0; i < countline; i++){
+        if (strcmp(instructions[i].label, string) == 0){
+            return i;
+        }
+    }
+    printf("error: undefined label\n");
+    exit(1);
+}
+
+void checkReg(char *reg){
+    if (!isNumber(reg)){
+        printf("error: no number register\n");
+        exit(1);
+    }
+    if (atoi(reg) < 0 || 7 < atoi(reg)){
+        printf("error: out of bound\n");
+        exit(1);
+    }
 }
